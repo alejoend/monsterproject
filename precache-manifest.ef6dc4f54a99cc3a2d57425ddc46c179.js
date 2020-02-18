ca562adbe2d881045e3a31cbcbca6d28010f1c1a
Alejandro <alejandro@merkdoo.com>
2020-02-18 16:30:17 -0500

    Updates

---
+++ precache-manifest.ef6dc4f54a99cc3a2d57425ddc46c179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "929f1cc8651b196515f086f29120fa2a",
+    "url": "/monsterproject/index.html"
+  },
+  {
+    "revision": "c5caf1aeab13f2a0499e",
+    "url": "/monsterproject/static/css/main.18758f38.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsterproject/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsterproject/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5caf1aeab13f2a0499e",
+    "url": "/monsterproject/static/js/main.a378b9f0.chunk.js"
+  },
+  {
+    "revision": "bee3bc91df95d7335b9e",
+    "url": "/monsterproject/static/js/runtime-main.fe1d0133.js"
+  }
+]);
